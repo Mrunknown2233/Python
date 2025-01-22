2998eb9bf5733c225ec6ff9dc20be244ca5eac82
Mrunknown2233 <dedhiasoumya299@gmail.com>
2025-01-22 23:08:05 +0530

    Added the basic codes

---
+++ accept&displayarray.py
@@ -0,0 +1,19 @@
+
+
+n = int(input("Enter the number : "))
+
+x = []
+
+for i in range(0,n):
+    # 2 ways to accept the array 
+    #x.append(int(input("Enter Number :")))
+    x+= [int(input("Enter the number : "))]
+
+print(x)
+
+
+mat1 = [[1,2,3],[4,5,6],[7,8,9]]
+
+print(mat1[1][2])
+
+
